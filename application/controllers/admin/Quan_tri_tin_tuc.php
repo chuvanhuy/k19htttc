89c38e50c7fff546ceaca0c2d3edc15949062d21
Chu Van Huy <chuvanhuy@gmail.com>
2020-04-06 22:39:04 +0700

    Mã nguồn ngày 01/04/2020

--- application/controllers/admin/Quan_tri_tin_tuc.php
+++ application/controllers/admin/Quan_tri_tin_tuc.php
@@ -18,6 +18,7 @@ class Quan_tri_tin_tuc extends CI_Controller {
 		$this->load->view('admin/v_quan_tri_tin_tuc', $data);
 	}
 
+	// Mục đích: Hiển thị form trống để nhập thông tin
 	public function them_moi_tin_tuc() {
 		// Thư viện URL
 		$this->load->helper('url');
@@ -26,6 +27,7 @@ class Quan_tri_tin_tuc extends CI_Controller {
 		$this->load->view('admin/v_tin_tuc_them_moi');
 	}
 
+	// Mục đích: thêm mới tin tức vào CSDL
 	public function thuc_hien_them_moi_tin_tuc() {
 		// Thư viện URL
 		$this->load->helper('url');
@@ -38,6 +40,66 @@ class Quan_tri_tin_tuc extends CI_Controller {
 
 		// Thực hiện thêm mới tin tức
 		$this->m_tin_tuc->them_moi_tin_tuc();
+
+		// Quay trở về trang Quản trị tin tức
+		redirect(base_url().'admin/quan_tri_tin_tuc.html');
+	}
+
+	// Mục đích: Chuẩn bị dữ liệu để sửa, hiển thị dữ liệu này trên FORM
+	public function sua() {
+		// Thư viện URL
+		$this->load->helper('url');
+
+		// Kết nối đến CSDL
+		$this->load->database();
+
+		// Lấy ID của tin tức mà cần sửa
+		$id = $this->uri->segment(4);
+
+		// Kết nối đến MODEL có tên m_tin_tuc
+		$this->load->model('m_tin_tuc');
+
+		// Lấy dữ liệu từ MODEL
+		 $data['tin_tuc'] = $this->m_tin_tuc->lay_tin_tuc_theo_ID($id);
+
+		// Đẩy dữ liệu tin tức đó ra form để các bạn cập nhật
+		$this->load->view('admin/v_tin_tuc_sua', $data);
+	}
+
+	// Mục đích: thực hiện cập nhật dữ liệu vào CSDL
+	public function thuc_hien_sua() {
+		// Thư viện URL
+		$this->load->helper('url');
+
+		// Kết nối đến CSDL
+		$this->load->database();
+
+		// Kết nối đến MODEL có tên m_tin_tuc
+		$this->load->model('m_tin_tuc');
+
+		// Thực hiện sửa tin tức
+    	$this->m_tin_tuc->cap_nhat_tin_tuc();
+
+		// Quay trở về trang Quản trị tin tức
+		redirect(base_url().'admin/quan_tri_tin_tuc.html');
+	}
+
+	// Mục đích: thực hiện xóa dữ liệu khỏi CSDL MySQL
+	public function xoa() {
+		// Thư viện URL
+		$this->load->helper('url');
+
+		// Kết nối đến CSDL
+		$this->load->database();
+
+		// Lấy ID của tin tức mà cần xóa
+		$id = $this->uri->segment(4);
+
+		// Kết nối đến MODEL có tên m_tin_tuc
+		$this->load->model('m_tin_tuc');
+
+		// Thực hiện xóa tin tức thông qua gọi hàm trong MODEL
+		$this->m_tin_tuc->xoa_tin_tuc($id);
 
 		// Quay trở về trang Quản trị tin tức
 		redirect(base_url().'admin/quan_tri_tin_tuc.html');
--- application/models/m_tin_tuc.php
+++ application/models/m_tin_tuc.php
@@ -11,6 +11,20 @@
             return $query->result();
         }
 
+
+        public function lay_tin_tuc_theo_ID($id)
+        {
+			// Lấy dữ liệu của tin tức đó
+			$query = $this->db->query("
+				SELECT *
+				FROM tbl_tin_tuc
+				WHERE id='".$id."'
+			");
+
+	       return $query->row();
+	    }
+
+
         public function them_moi_tin_tuc()
         {
         	// Lấy dữ liệu trả về
@@ -33,12 +47,31 @@
 
         public function cap_nhat_tin_tuc()
         {
-        	// Từ từ đã
+			// Lấy dữ liệu trả về
+	    	$id = $_POST['txtID'];
+			$loai_tin_id = '1';
+			$tieu_de = $_POST['txtTieuDe'];
+			$mo_ta = $_POST['txtMoTa'];
+			$noi_dung = $_POST['txtNoiDung'];
+
+			// Chuẩn bị dữ liệu chèn vào bàng trong CSDL
+			$data = array(
+		        'loai_tin_id' => $loai_tin_id,
+		        'tieu_de' => $tieu_de,
+		        'mo_ta' => $mo_ta,
+		        'noi_dung' => $noi_dung
+			);
+
+			// Thực hiện cập nhật dữ liệu vào bảng TIN TỨC trong CSDL
+			$this->db->where('id', $id);
+			$this->db->update('tbl_tin_tuc', $data);
         }
 
-        public function xoa_tin_tuc()
+        public function xoa_tin_tuc($id)
         {
-        	// Từ từ đã
+        	// Thực hiện xóa tin tức
+			$this->db->where('id', $id);
+			$this->db->delete('tbl_tin_tuc');
         }
 	}
 ;?>
